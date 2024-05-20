ee8e2ecbe544cfa7d7206d931b5c323d26f04bb5
GustavoJannuzzi <gujannuzzi17@gmail.com>
2024-05-20 18:14:55 -0300

    first commit

---
+++ frontend/notes-app/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,78 @@
+import React, { useState, FormEvent } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import PasswordInput from "../../components/Input/PasswordInput";
+import { validateEmail } from "../../utils/helper"; // Certifique-se de que o caminho para helper está correto
+
+
+const SignUp: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSignUp = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if(!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("")
+
+    //Singup API Call
+
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">Sign Up</h4>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box mt-4"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            
+            <button type="submit" className="btn-primary mt-4">
+              Create Account
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Already have an account? {" "}
+              <Link to="/login" className="font-medium text-primary underline">
+                Login
+              </Link>
+            </p>
+
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
