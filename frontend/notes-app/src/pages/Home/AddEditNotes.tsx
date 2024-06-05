6cf9903045565b0d5c125985b81381b27c3127ea
GustavoJannuzzi <gujannuzzi17@gmail.com>
2024-06-05 15:51:28 -0300

    TagInput errorhandler

--- frontend/notes-app/src/pages/Home/AddEditNotes.tsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.tsx
@@ -1,42 +1,96 @@
 import React, { useState } from "react";
 import TagInput from "../../components/Input/TagInput";
+import { MdClose } from "react-icons/md";
 
-const AddEditNotes: React.FC = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+interface AddEditNotesProps {
+  onClose: () => void;
+  noteData: (data: { title: string; content: string; tags: string[] }) => void; // Define the correct type for noteData
+  type: "add" | "edit"; // Define type as a string with specific values
+}
+
+const AddEditNotes: React.FC<AddEditNotesProps> = ({ noteData, type, onClose }) => {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
   const [tags, setTags] = useState<string[]>([]);
 
+  const [error, setError] = useState<string | null>(null);
+
+  // Add Note
+  const addNewNote = async () => {
+    // Add logic to handle adding a new note
+    noteData({ title, content, tags });
+    onClose();
+  };
+
+  // Edit Note
+  const editNote = async () => {
+    // Add logic to handle editing an existing note
+    noteData({ title, content, tags });
+    onClose();
+  };
+
+  const handleAddNote = () => {
+    if (!title) {
+      setError("Please enter the title");
+      return;
+    }
+
+    if (!content) {
+      setError("Please enter the content");
+      return;
+    }
+
+    setError(null);
+
+    if (type === "edit") {
+      editNote();
+    } else {
+      addNewNote();
+    }
+  };
+
   return (
-    <div>
-      <div className="flex flex-col gap-2">
-        <label className="input-label">TITLE</label>
-        <input
-          type="text"
-          className="text-2xl text-slate-950 outline-none"
-          placeholder="Go To Gym At 5"
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      </div>
-      <div className="flex flex-col gap-2 mt-4">
-        <label className="input-label">CONTENT</label>
-        <textarea
-          className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
-          placeholder="Content"
-          rows={10}
-          value={content}
-          onChange={({ target }) => setContent(target.value)}
-        />
-      </div>
+    <div className="relative">
+      <div className="modal-content">
+        <button
+          className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50"
+          onClick={onClose}
+        >
+          <MdClose className="text-xl text-slate-400" />
+        </button>
 
-      <div className="mt-3">
-        <label className="input-label">TAGS</label>
-        <TagInput tags={tags} setTags={setTags} />
-      </div>
+        <div className="flex flex-col gap-2">
+          <label className="input-label">TITLE</label>
+          <input
+            type="text"
+            className="text-2xl text-slate-950 outline-none"
+            placeholder="Go To Gym At 5"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-2 mt-4">
+          <label className="input-label">CONTENT</label>
+          <textarea
+            className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+            placeholder="Content"
+            rows={10}
+            value={content}
+            onChange={({ target }) => setContent(target.value)}
+          />
+        </div>
 
-      <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
-        ADD
-      </button>
+        <div className="mt-3">
+          <label className="input-label">TAGS</label>
+          <TagInput tags={tags} setTags={setTags} />
+        </div>
+
+        {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+
+        <button className="btn-primary font-medium mt-5 p-3" onClick={handleAddNote}>
+          {type === "edit" ? "EDIT" : "ADD"}
+        </button>
+      </div>
     </div>
   );
 };
